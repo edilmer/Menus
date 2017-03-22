589bd5bee61e18ed5651878e7cb90edeed9bd44e
aula7 <pc715@gmail.com>
2017-03-22 13:14:47 -0500

    Initial commit

---
+++ app/src/main/java/com/example/aula7/menus/LoginActivity.java
@@ -0,0 +1,29 @@
+package com.example.aula7.menus;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.View;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        onConfigToolbar();
+    }
+
+    public void onConfigToolbar(){
+        Toolbar toolbar = (Toolbar ) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+}
